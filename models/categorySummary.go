696c8e2570aa5d39157dffd23c02c1820fbe8de2
Amadeusz07 <lechniak.amadeusz07@gmail.com>
2020-03-08 11:39:18 +0100

    Changed sum type to float64

--- models/categorySummary.go
+++ models/categorySummary.go
@@ -5,5 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type CategorySummary struct {
 	ID           primitive.ObjectID `json:"categoryId,omitempty" bson:"_id,omitempty"`
 	CategoryName string             `json:"categoryName,omitempty"`
-	Sum          float32            `json:"sum,omitempty"`
+	Sum          float64            `json:"sum,omitempty"`
 }
